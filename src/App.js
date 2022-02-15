ca9a74296d0db472400f684cf13d601f2e6c2d99
Santiago López <tsgustavo1@gmail.com>
2022-02-14 22:43:27 -0500

    Corrections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
+/* import { BrowserRouter as  Routes, Route } from 'react-router-dom'; */
 import Home from "./components/Pages/Home/Home";
-import Product from "./components/Pages/Product/Product";
+/* import Product from "./components/Pages/Product/Product"; */
 import NavBar from './components/Containers/NavBar/NavBar';
 import Footer from "./components/Containers/Fotter/Footer";
 
